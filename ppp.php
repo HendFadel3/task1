df184385788d56822cd3b6ac480f206dcb5cc0a5
HendFadel3 <95578394+HendFadel3@users.noreply.github.com>
2021-12-05 11:23:44 -0800

    Update ppp.php

--- ppp.php
+++ ppp.php
@@ -7,7 +7,7 @@ if($unit>0 && $unit<=50)
         $bill=$unit*3.50;
         echo $bill;
     }
-    elseif($unit>100 && $unit<=150)
+    elseif($unit>50 && $unit<=150)
     {
         $bill=$unit*4.00;
         echo $bill;
