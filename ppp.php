b5531b88ae3aad41c4673df3bbb418dcc97ac090
HendFadel3 <95578394+HendFadel3@users.noreply.github.com>
2021-12-06 22:15:15 +0200

    Update ppp.php

--- ppp.php
+++ ppp.php
@@ -1,29 +1,52 @@
+
 <?php
-
-$unit=118;
-$bill=0;
-if($unit>0 && $unit<=50)
-    {
-        $bill=$unit*3.50;
-        echo $bill;
-    }
-    elseif($unit>50 && $unit<=150)
-    {
-        $firstPart=50*3.50;
-        $remainUnitPrice=($unit-50)*4.00;
-        $bill= $firstPart + $remainUnitPrice ;
-        echo $bill;
-    }
-else{
-        $firstPart=50*3.50;
-        $secondPart=100*4.00;
-        $remainUnitPrice=($unit-150)*6.50;
-        $bill=$firstPart+$secondPart+$remainUnitPrice;
-        echo $bill;
-    
-    }
-
-
- 
-
+$cha="z";
+//$cha="a";
+$cha ++;
+if(strlen($cha)>1){
+    echo $cha[0];
+}
+else {
+    echo $cha;
+}
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//<?php
+//$unit=118;
+//$bill=0;
+//if($unit>0 && $unit<=50)
+//    {
+//        $bill=$unit*3.50;
+//        echo $bill;
+//    }
+//   elseif($unit>50 && $unit<=150)
+//    {
+//        $firstPart=50*3.50;
+//        $remainUnitPrice=($unit-50)*4.00;
+//        $bill= $firstPart + $remainUnitPrice ;
+//        echo $bill;
+//    }
+//else{
+//        $firstPart=50*3.50;
+//        $secondPart=100*4.00;
+//        $remainUnitPrice=($unit-150)*6.50;
+//          $bill=$firstPart+$secondPart+$remainUnitPrice;
+//        echo $bill;
+    
+//    }
+
+// ?>
